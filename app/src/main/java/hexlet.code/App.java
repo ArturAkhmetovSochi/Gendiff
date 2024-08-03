f8c964a6338d60e697c5edccba121c710fc9ad1c
Artur Akhmetov <artur.akh@internet.ru>
2024-08-03 10:44:53 +0300

    complete 3rd and 4th task in project

--- app/src/main/java/hexlet.code/App.java
+++ app/src/main/java/hexlet.code/App.java
@@ -1,8 +1,32 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares two configuration files and shows a difference.")
+
+public class App implements Callable<Integer> {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 
+    @Parameters(description = "path to first file")
+    String filepath1;
+
+    @Parameters(description = "path to second file.")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    String format = "stylish";
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Hello, World!");
+        return 0;
+    }
 }
