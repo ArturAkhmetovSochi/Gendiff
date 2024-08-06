9f791ad3063f931565fcd9c4e9ffcfc64928de17
Artur Akhmetov <artur.akh@internet.ru>
2024-08-06 15:56:01 +0300

    1

---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.gradle.kotlin.dsl.support.kotlinCompilerOptions
+
+plugins {
+    checkstyle
+    application
+    jacoco
+    id("checkstyle")
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "io.hexlet"
+
+version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
+
+repositories { mavenCentral() }
+
+dependencies {
+    implementation("info.picocli:picocli:4.7.6")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.6")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.8.9")
+    implementation("io.vertx:vertx-core:3.5.3")
+    testImplementation("org.assertj:assertj-core:3.22.0")
+    }
+
+tasks.test {
+    useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
+
+fun compileJava(function: () -> Unit) {
+
+}
