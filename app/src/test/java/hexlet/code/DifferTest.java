4ab501007ae2617172f72e3092fa52236763d393
Artur Akhmetov <artur.akh@internet.ru>
2024-08-07 11:01:10 +0300

    correct linter checkstyle for DifferTest

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -10,22 +10,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DifferTest {
 
     @Test
-    public void testMakeMapFromPath () throws IOException {
+    public void testMakeMapFromPath() throws IOException {
 
         String filepath = "/home/artur/java-project-71/app/src/test/resources/testFile1.json";
         Map<String, Object> map1 = Map.of("host", "hexlet.io", "timeout", 25,
                 "proxy", "777.777.777", "follow", true);
         Map<String, Object> map2 = Differ.makeMapFromPath(filepath);
 
-        Assertions.assertTrue( map1.equals(map2) );
+        Assertions.assertTrue(map1.equals(map2));
     }
 
     @Test
-    public void testGenerate () throws IOException {
+    public void testGenerate() throws IOException {
 
-    String str1 = Differ.generate("/home/artur/java-project-71/app/src/test/resources/testFile1.json", "/home/artur/java-project-71/app/src/test/resources/testFile2.json");
-    String str2 = ("-follow:true" +"\n" + " host:hexlet.io" + "\n" + "-proxy:777.777.777" +"\n" + "-timeout:25" +"\n" + "+timeout:55" +"\n" + "+verbose:false" + "\n");
+        String str1 = Differ.generate("/home/artur/java-project-71/app/src/test/resources/testFile1.json",
+            "/home/artur/java-project-71/app/src/test/resources/testFile2.json");
+        String str2 = ("-follow:true" + "\n" + " host:hexlet.io" + "\n" + "-proxy:777.777.777"
+            + "\n" + "-timeout:25" + "\n" + "+timeout:55" + "\n" + "+verbose:false" + "\n");
 
-    assertEquals(str1, str2);
+        assertEquals(str1, str2);
     }
 }
